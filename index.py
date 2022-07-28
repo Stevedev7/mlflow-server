78b9db1c315a899edae8447c73b748680d86dcdd
Steve Pinto <brian.pinto@niveussolutions.com>
2022-07-28 17:00:13 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+import os
+
+import mlflow
+
+mlflow_uri = "http://localhost:3000" # change to your uri + port
+
+mlflow.set_tracking_uri(mlflow_uri)
+
+exp_id = mlflow.set_experiment("mlflow_test1")
+
+mlflow.log_param("param1", "value1")
+mlflow.log_param("param2", "value2")
+
+for i in range(10):
+    mlflow.log_metric("metric1", value=1/(i+1), step=i)
+    
+mlflow.log_artifact('./requirements.txt')
+mlflow.end_run()
